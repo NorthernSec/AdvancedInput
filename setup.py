f91bd84ca9ae73c0b456361828edbf4136f60783
Pieter-Jan Moreels <pieterjan.moreels@gmail.com>
2016-10-11 23:31:56 +0200

    Add install script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='AdvancedInput',
+      version='0.1',
+      description='built-in input function replacement',
+      author='Pieter-Jan Moreels',
+      url='https://github.com/NorthernSec/AdvancedInput',
+      packages=['AdvancedInput'],
+      license="Modified BSD license",
+)
